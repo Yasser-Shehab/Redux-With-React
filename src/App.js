047dba35871213ba96cab89c56095ca8454ea0bb
AquaApple <yassershehab9@gmail.com>
2022-09-06 17:14:54 +0200

    Implemented Auth to components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import Counter from "./components/Counter";
 import Header from "./components/Header";
 import Auth from "./components/Auth";
+import { useSelector, useDispatch } from "react-redux";
+import { authActions } from "./store/index";
+import UserProfile from "./components/UserProfile";
 
 function App() {
+  const dispatch = useDispatch();
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
+
+  const loginHandle = (e) => {
+    e.preventDefault();
+    dispatch(authActions.login());
+  };
+
   return (
     <>
       <Header />
-      <Auth />
+      {isAuth ? <UserProfile /> : <Auth onLogin={loginHandle} />}
       <Counter />
     </>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,28 @@
-import classes from './Header.module.css';
+import classes from "./Header.module.css";
+import { useSelector, useDispatch } from "react-redux";
+import { authActions } from "../store";
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
+
+  const logoutHandle = () => {
+    dispatch(authActions.logout());
+  };
+
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
       <nav>
         <ul>
           <li>
-            <a href='/'>My Products</a>
+            <a href="/">My Products</a>
           </li>
           <li>
-            <a href='/'>My Sales</a>
+            <a href="/">My Sales</a>
           </li>
           <li>
-            <button>Logout</button>
+            {isAuth ? <button onClick={logoutHandle}>Logout</button> : <button>Login</button>}
           </li>
         </ul>
       </nav>
